acff6a23ea56f81a738ed76f5f2e5b385a69947a
adolfobello <adolfobello@tuyoya.com>
2016-05-27 18:25:50 -0400

    Funcionando par o impar

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # Este es el punto de entrada al sistema
 print("Hello world")
+# Pide un numero. Supone que lo es.
+n = int(input("Que numero quiere saber si es par: "))
+if (n%2) == 0:
+    print("El numero %s es par" % n )
+else:
+    print("El numero %s es impar" % n )
